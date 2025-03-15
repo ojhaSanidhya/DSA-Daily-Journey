0afd88b1ac0ddb5023058e8ab81b69ca6a301c43
ojhaSanidhya <132838074+ojhaSanidhya@users.noreply.github.com>
2025-03-15 21:19:49 +0530

    Update Basics_01.cpp
    Update Basics_01

---
+++ Basics_&_Pattern_Printing/Basics_01.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"Sanidhya"<<endl;cout<<"Ojha"<<'\n';
+
+    // int -> 4 byte
+    // int num = 50;
+    // cout<<num<<endl;
+    // cout<<sizeof(num)<<endl;
+
+    //character -> 1byte         
+    // char ch = 'S';
+	// cout << ch << endl;
+	// cout << sizeof(ch) << endl;
+
+    // Note >> We don't have to use double inverted comma, for assigning any character we only use single inverted comma.
+
+    //Float -> 4byte
+    // float point = 1.69;
+    // cout<< point <<endl;
+    // cout << sizeof(point)<<endl;
+
+    //Boolean -> 1byte
+    // bool Check = true;
+    // cout << Check <<endl;
+    // cout << sizeof(Check)<<endl;
+
+    //long -> 4byte
+    // long number = 87;
+    // cout<< number <<endl;
+    // cout<< sizeof(number)<<endl;
+
+    //OPERATORS
+
+    // int a = 3;
+	// int b = 2;
+
+	// cout << a + b << endl;
+	// cout << a - b << endl;
+	// cout << a*b << endl;
+	//cout << a/b << endl;
+	//cout << b % a << endl; 
+
+    // Garbage Value > Value which comes randomly for eg.
+    // int Ojha;
+    // cout<<Ojha<<endl;
+
+    // cout << (a == b) << endl;
+
+    // int age = 21;
+    // int Father_age = 58;
+
+    // if(age>=18 && Father_age<60){
+    //     cout<<"You are able to drink"<<endl; 
+
+        // Note > In cout we only use double inverted comma.
+    
+    //Input from User end
+
+    // int marks;
+    // cout<< "Enter your marks here: "<<endl;
+    // cin>>marks;
+    // cout<< "Your marks is: "<< marks<<endl;
+
+    // string Name;
+    // cout<<"What is your name: "<<endl;
+    // cin>>Name;
+    // cout<<"Your name is: "<<Name<<endl;
+
+    };
+
+    
