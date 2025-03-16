aa72fb9058208ce4bf4588002eda9c909cf3fb48
Sanidhya Ojha <132838074+ojhaSanidhya@users.noreply.github.com>
2025-03-16 13:41:25 +0530

    Create Basics_02.cpp

---
+++ Basics_&_Pattern_Printing/Basics_02.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//Hollw Rectangle
+    // for (int row = 0; row < 5; row++)
+    // {
+    //     for (int col = 0; col < 5; col++)
+    //     {
+    //         if (row==0 || row==4)
+    //         {
+    //             cout<<"* ";
+    //         }
+
+    //         else
+    //         {
+    //             if (col==0 || col==4)
+    //             {
+    //                 cout<<"* ";
+    //             }
+    //             else
+    //             {
+    //                 cout<<"  ";
+    //             }
+                
+    //         }
+            
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+//Half pyramid
+
+// for (int row = 0; row < 10; row++)
+// {
+//     for (int col = 0; col < row+1; col++)
+//     {
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+// }
+
+//Inverted Half pyramid
+
+// int a;
+// cin>>a;
+// for (int row = 0; row < a; row++)
+// {
+//     for (int col = 0; col < a-row; col++)
+//     {
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+    
+// }
+
+// Numeric Half pyramid
+
+// int b;
+// cin>>b;
+// for (int row = 0; row < b; row++)
+// {
+//     for (int col = 0; col < row+1; col++)
+//     {
+//         cout<<col+1;
+//     }
+//     cout<<endl;
+// }
+
+//NUmeric Inverted Half pyramid
+
+// int a;
+// cin>>a;
+// for (int row = 0; row < a; row++)
+// {
+//     for (int col = 0; col < a-row; col++)
+//     {
+//         cout<<col+1;
+//     }
+//     cout<<endl;
+    
+// }
+
+    
+}
+
