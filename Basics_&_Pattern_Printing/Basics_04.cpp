d058bd709b79dc9c98f24b401ad39a77cb394fd3
Sanidhya Ojha <132838074+ojhaSanidhya@users.noreply.github.com>
2025-03-18 20:23:10 +0530

    Create Basics_04.cpp

---
+++ Basics_&_Pattern_Printing/Basics_04.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 5;
+    // int b = 10;
+
+    // cout << (a | b) << endl;
+
+    // //HW
+    bool num = 1;
+    // cout << ~num << endl;
+    // cout << (~num) << endl;
+    // cout << ~(num) << endl;
+
+    // int a = 5;
+    // int b = -5;
+
+    // cout << (a^b) << endl; // find the 2's compliment of the ans => Ans
+
+    //     int a = 7;
+    // 	int ans = (a << 2);
+    // cout << ans << endl;
+
+    // unsigned int n = -10;
+    // int ans = (n >> 1) ;
+    // cout << ( n >> 1);
+
+    // int n = 10;
+    // cout << (n << -1);
+
+    // int a = 21;
+    // cout << ++a << endl;
+    // //a -> 22
+    // cout << a++ << endl;
+    // //verify krte h inc hua ya nahi
+    // cout << a << endl;
+
+    // int a = 10;
+    // cout << (++a)*10 << endl;
+
+    // cout << (a++)*10 << endl;
+
+    // cout << a ;
+
+    // int a = 10;
+    // cout << ((a++)*(++a)) << endl;
+
+    // for(int i=0; i<5; i++) {
+    // 	cout << i << endl;
+    // 	if(i == 3)
+    // 		break;
+    // }
+
+    // for(int i=0; i<=5; i++) {
+
+    // 	cout << "Hello Jee" << endl;
+    // 	cout << "Kaise ho saare" << endl;
+    // 	continue;
+    // }
+
+    // int a = 5;
+    // int b;
+
+    // a = 25;
+    // cout << a << endl;
+
+    // int andy = 10; // We are allowed to update the variable not to reassigned
+
+    // Local Variable
+    //  for(int i=0; i<5; i++) {
+    //  	cout << i << endl;
+    //  }
+
+    // cout << "or bhai i kya value h teri" << i << endl; // Here i is declared in loop
+
+    // if(true) {
+    // 	int a = 25;
+    // 	cout << a << endl;
+    // }
+
+    // cout << a << endl;
+
+    // int a = 101;
+    // if(true) {
+    // 	cout << "inside if number 1" << endl;
+    // 	//int a = 202;
+    // 	if(true) {
+    // 		cout << "inside if number 2" << endl;
+    // 		//int a = 303;
+    // 		cout << a << endl;
+    // 	}
+    // }
+
+    // cout << age << endl;
+
+    // return 0;
+}
